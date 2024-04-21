d02a065327128e7b14e9e73e01b67fcdee5ba0bb
Ablogin Egor <47031969+egor2810@users.noreply.github.com>
2024-04-21 11:50:49 +0300

    Initial Commit

---
+++ Netflix UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix UIKit
+//
+//  Created by Егор Аблогин on 21.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
